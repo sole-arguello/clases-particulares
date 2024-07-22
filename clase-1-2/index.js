10f2eea797a276ede0974f7dde3fb35614db76dc
Arguello Maria Soledad Eugenia <soledad.ar1@gmail.com>
2024-07-22 09:09:36 -0300

    first commit

---
+++ clase-1-2/index.js
@@ -0,0 +1,68 @@
+// const dia = ["texto", 2 , true]
+// // // indice =     0     1     2 ......
+
+// // dia.push("32")
+
+
+//  console.log(dia)
+
+// let texto = ' '
+// let nombre = ""
+// console.log(nombre.length) 
+// let num = 2 + ` ${nombre}`
+// console.log(num)
+// texto = " lslldsls " 
+
+// texto = ` llaldslsl ${ nombre } ${ num } `
+
+// console.log(texto)
+
+// let array = [ ]
+// console.log("Esto esta vacio: ",array)
+
+// let algo = true
+// console.log(typeof(algo))
+
+// // [24, 15, 2, 6]
+
+// const par = 15 % 3
+
+// console.log(par)
+
+
+// // estrictos === !==
+// // no estrictos == !=
+
+// const num = 15
+// console.log(typeof(num))
+// const num2 = "15"
+// console.log(typeof(num2))
+
+// acceder [numeri de indice] i [ i ]
+
+// console.log(dia[4])
+
+// //objetos { clave : valor }
+
+// const objeto = {
+//     nombre : "jose",
+//     edad : 22
+//  }
+//  console.log(objeto.nombre)
+//  console.log(objeto["nombre"])
+
+//  console.log(Object.keys(objeto))
+//  console.log(Object.values(objeto))
+//  console.log(Object.entries(objeto))
+//  console.log(Object.entries(objeto)[0][1])
+
+ const array2 = [
+    { nombre : "jose", edad : 22 },//0
+    { nombre : "juan", edad : 23 }//1
+ ]
+ //          objeto.nombre
+ console.log(array2.push)
+
+ const VARIABLE = 3.14
+
+
