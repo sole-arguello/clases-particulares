74f22e784f74f549d9ce8ea1ea98ce64f83678c5
Arguello Maria Soledad Eugenia <soledad.ar1@gmail.com>
2024-08-06 23:42:14 -0300

    seguimos reforzando for

---
+++ clase-4/index.js
@@ -0,0 +1,66 @@
+const numeros = [25, 45, 10, 2, 8]
+// incide =      0 1 2 3 4 5 6 7 8 9
+
+// for(let i=1; i < numeros.length; i++){
+//     console.log("El cuadrodo es: ", numeros[i] * numeros[i])
+//     console.log("El cuadrodo es: ", i * i)
+// }
+
+
+
+
+// //array -> numeros[i] * numeros[i]
+// // [0] = 25
+// // [1] = 45
+// // [2] = 10
+// // [3] = 2
+// // [4] = 8
+
+
+// const palabras = [ "uno", "dos", "lechuga", "bicicleta",  "nueve", "diez" ]
+
+// let palabraNueva = []
+// for(let i=0; i < palabras.length; i++){
+
+//     if(palabras[i].length > 5){
+//         console.log(palabras[i].length)
+//         palabraNueva.push(palabras[i])
+
+//     }
+// }
+
+// console.log(palabraNueva)
+
+
+// const frase = "HolA, 2 como estas?"
+
+// const vocales = ["a", "e", "i", "o", "u"]
+// let vocalesEncontradas = []
+// let consontesEncontradas = []
+// let contadorVocales = 0
+// let contadorConsonantes = 0
+
+// for(i=0; i < frase.length; i++){
+
+//     let caracteres = frase[i].toLocaleLowerCase()
+//     if(caracteres >= "a" && caracteres <= "z"){
+//         //console.log(caracteres)
+
+//         if(vocales.includes(caracteres)){
+//             vocalesEncontradas.push(caracteres)
+//             contadorVocales++
+//         }else{
+//             consontesEncontradas.push(caracteres)
+//             contadorConsonantes++
+//         }
+
+//     }
+
+
+
+// }
+// console.log(vocalesEncontradas)
+// console.log(consontesEncontradas)
+// console.log("La cantidad de vocales", contadorVocales)
+// console.log("La cantidad de consonantes", contadorConsonantes)
+
