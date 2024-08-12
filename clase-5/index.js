14d81583c9d14d2301a41f08e057579c9bfb7954
Arguello Maria Soledad Eugenia <soledad.ar1@gmail.com>
2024-08-12 08:21:49 -0300

    cerramos tema con for... of ... in

---
+++ clase-5/index.js
@@ -0,0 +1,53 @@
+//for clasico
+const numeros = [25, 5, 10, 2, 8]
+
+// for(let i = 0; i < arrayNumeros.length; i++){
+
+//     //console.log(arrayNumeros[i])
+
+// }
+
+//for...in itera sobre las propiedades(claves) de un objeto, (en el caso de un array, sobre los índices)
+
+//sintaxis
+// for(let i in array){
+
+//     console.log(array[i])
+
+// }
+
+for(let numero in numeros){
+    console.log(numeros[numero])
+}
+
+
+//for...of itera sobre los valores de un objeto iterable como arrays, strings, etc.
+
+//sintaxis
+// for(let variable of array){
+//     console.log(variable)
+// }
+
+
+// for(let numero of arrayNumeros){
+
+//     console.log(numero)
+
+// }
+
+// //entries (clave -valor )
+
+// for(let [i, valor] of arrayNumeros.entries()){
+
+//     console.log(`i: ${i}, valolr: ${valor}`)
+// }
+
+
+
+
+
+
+
+
+
+
